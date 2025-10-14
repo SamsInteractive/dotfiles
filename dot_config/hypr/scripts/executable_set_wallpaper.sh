b45b1647db89b6e403eb26f659c8f4c78f99a7cc
Sam Walker <190484284+SamsInteractive@users.noreply.github.com>
2025-10-14 23:54:20 +1000

    setup current_wallpaper export for cross script use

--- dot_config/hypr/scripts/executable_set_wallpaper.sh
+++ dot_config/hypr/scripts/executable_set_wallpaper.sh
@@ -6,7 +6,20 @@ if [ "$#" -ne 1 ]; then
 fi
 
 wallpaper="$1"
-transitions=(wipe center outer any)
+settings_dir="$HOME/.config/hypr/settings"
+current_wallpaper="$settings_dir/current_wallpaper"
+
+if [ ! -d "$settings_dir" ]; then
+    mkdir -p "$settings_dir"
+fi
+
+
+transitions=(wipe any center outer any)
 transition=${transitions[$RANDOM  % ${#transitions[@]}]}
 
 swww img "$wallpaper" --transition-fps 60 --transition-step 255 --transition-type "$transition"
+
+if [ ! -f "$current_wallpaper" ]; then
+    touch "$current_wallpaper"
+fi
+echo "$wallpaper" >"$current_wallpaper"
