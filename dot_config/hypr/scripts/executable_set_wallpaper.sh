3114778c2cabce41d525120dbd54d02801e77a09
Sam Walker <190484284+SamsInteractive@users.noreply.github.com>
2025-10-18 01:48:37 +1000

    chore: resolved variable parsing for eza alias, changed current_wallpaper to symlink, minor formatting changes

--- dot_config/hypr/scripts/executable_set_wallpaper.sh
+++ dot_config/hypr/scripts/executable_set_wallpaper.sh
@@ -9,17 +9,18 @@ wallpaper="$1"
 settings_dir="$HOME/.config/hypr/settings"
 current_wallpaper="$settings_dir/current_wallpaper"
 
+TRANSITIONS=(wipe any center outer any)
+transition=${TRANSITIONS[$RANDOM  % ${#TRANSITIONS[@]}]}
+
 if [ ! -d "$settings_dir" ]; then
     mkdir -p "$settings_dir"
 fi
 
-
-transitions=(wipe any center outer any)
-transition=${transitions[$RANDOM  % ${#transitions[@]}]}
-
-swww img "$wallpaper" --transition-fps 60 --transition-step 255 --transition-type "$transition"
-
 if [ ! -f "$current_wallpaper" ]; then
     touch "$current_wallpaper"
 fi
-echo "$wallpaper" >"$current_wallpaper"
+
+ln -sf "$wallpaper" "$current_wallpaper"
+
+
+swww img "$wallpaper" --transition-fps 60 --transition-step 255 --transition-type "$transition"
