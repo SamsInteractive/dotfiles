f1fd288b8c87d546b806a0dbb540bf1a1f813dd9
Sam Walker <190484284+SamsInteractive@users.noreply.github.com>
2025-11-04 01:57:54 +1000

    changes: adjusted how wallpapers are displayed in rofi

--- dot_config/hypr/scripts/executable_wallpaper_switcher.sh
+++ dot_config/hypr/scripts/executable_wallpaper_switcher.sh
@@ -20,8 +20,38 @@ else
 			absolute="$PWD/${wallpaper#./}"
 			printf '%s\0display\x1f%s\x1ficon\x1f%s\n' "$absolute" "$displayname" "$absolute"
     	done | rofi -dmenu -i -p "" -show-icons \
-		-theme-str 'element-icon { size: 64px; }'
+		-theme-str 'element { orientation: vertical; padding: 6px; margin: 2px; }
+		listview {
+			columns: 3;
+			lines: 3;
+			dynamic: true;
+			fixed-height: true;
+			fixed-columns: false;
+			spacing: 4px;
+			scrollbar: false;
+			layout: vertical;
+		}
+
+		element-icon {
+			size: 160px;
+			scale: true;
+			padding: -30px 0 -30px 0;
+			horizontal-align: 0.5;
+			vertical-align: 0.5;
+		}
+
+		element-text {
+			padding: 0px;
+			horizontal-align: 0.5;
+		}
+
+		window { height: 50%; width: 50%; }
+
+		/* old - element-icon { size: 64px; } */
+		'
 	)
+
+
 
 	if [ -n "$selected" ]; then
     	$HOME/.config/hypr/scripts/set_wallpaper.sh "$selected"
