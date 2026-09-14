379b47500c7f27576e52e3f74e4436ef8928d895
Sam Walker <190484284+SamsInteractive@users.noreply.github.com>
2026-09-14 22:13:36 +1000

    LUA migration: converted existing Hyprland animations to LUA; split animation configs to match new default sourcing format

---
+++ dot_config/hypr/animations.lua
@@ -0,0 +1,17 @@
+--     ___          _                 __  _
+--    /   |  ____  (_)___ ___  ____ _/ /_(_)___  ____  _____
+--   / /| | / __ \/ / __ `__ \/ __ `/ __/ / __ \/ __ \/ ___/
+--  / ___ |/ / / / / / / / / / /_/ / /_/ / /_/ / / / (__  )
+-- /_/  |_/_/ /_/_/_/ /_/ /_/\__,_/\__/_/\____/_/ /_/____/
+--
+-- Attempt to source hostname config
+local success = pcall(require, "conf/animations/saved/" .. HOSTNAME)
+
+-- Source default config if hostname config failed to be sourced or
+-- ALWAYS_SOURCE_DEFAULT is true. Will not run if overridden in config.lua
+if (not success or config.ALWAYS_SOURCE_DEFAULT) and not config.ANIM_OVERRIDE then
+	require("conf/animations/default")
+end
+
+-- Source any custom user configs
+pcall(require, "conf/animations/user")
